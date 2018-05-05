e801633c0f59e57ccb5e83eb29b11fc4d7ad43ab
Vikram Cothur <vcothur7@gmail.com>
2018-05-05 22:08:52 +0530

    CHANGE

--- Sketches/SpaceInvaders/main.js
+++ Sketches/SpaceInvaders/main.js
@@ -214,10 +214,10 @@ function buttonsIfMobile()
     if (isMobile) {
         Lbutton = createButton("Left");
         Lbutton.position(0,height/1.2);
-        Lbutton.mousePressed(()=>{player.setForce(-width/100)});
+        Lbutton.mousePressed(()=>{player.setForce(-1)});
         Rbutton = createButton("Right");
         Rbutton.position(width/1.2,height/1.2);
-        Rbutton.mousePressed(()=>{player.setForce(width/100)});
+        Rbutton.mousePressed(()=>{player.setForce(1)});
     }
 }
 //EVENTS
