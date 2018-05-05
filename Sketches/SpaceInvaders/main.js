4a8fde2e12c258de8b54f006e0f46800c7d40712
Vikram Cothur <vcothur7@gmail.com>
2018-05-05 22:28:40 +0530

    game over text

--- Sketches/SpaceInvaders/main.js
+++ Sketches/SpaceInvaders/main.js
@@ -53,7 +53,7 @@ function draw()
         noLoop();
         image(gameOverImage,0,0,width,height);
         fill(203, 63, 12);//#cb4015
-        text("SCORE: "+score,270,290);
+        text("SCORE: "+score,width/2,height/1.2);
     }
     if(monsters.length==0)
     {        
