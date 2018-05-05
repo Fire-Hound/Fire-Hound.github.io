f0494edcc4ec46a5e73c7c466c5c9ae045211972
Vikram Cothur <vcothur7@gmail.com>
2018-05-05 20:17:07 +0530

    hu
    u

---
+++ resizeScript.js
@@ -0,0 +1,5 @@
+window.onresize = ()=>{
+    // document.getElementById("defaultCanvas0").height = window.innerHeight;
+    // document.getElementById("defaultCanvas0").width = window.innerWidth;
+    window.location.reload(true)
+}
