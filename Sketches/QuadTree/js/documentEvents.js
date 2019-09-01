66648817dcb08589828716b2076894bfa4d65a00
Vikram Cothur <vcothur7@gmail.com>
2019-09-01 13:54:38 +0530

    Update documentEvents.js

--- Sketches/QuadTree/js/documentEvents.js
+++ Sketches/QuadTree/js/documentEvents.js
@@ -1,5 +1,7 @@
 
 document.onclick = (ev)=>{
     document.getElementsByClassName("instruction")[0].style.opacity = 0;
+    document.getElementsByClassName("instruction")[0].style.display = "none";
+
 }
 
